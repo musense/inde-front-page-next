07986a0dcb83833f2b3724afe53a31d03a231069
musense <musense.ad@gmail.com>
2023-06-15 17:27:27 +0800

    1. 塞ga code 2. yarn add sharp

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,26 @@
 import { Context } from '@store/context';
 import '@styles/global.css';
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Script
+        src='https://www.googletagmanager.com/gtag/js?id=G-TJ54RSE86S'
+        strategy='afterInteractive'
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+      
+            gtag('config', 'G-TJ54RSE86S');
+        `}
+      </Script>
       <Context>
         <Component {...pageProps} />
       </Context>
